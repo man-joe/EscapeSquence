1e00df9403d98aa830667ad5be4dccdeecf74798
man-joe <jhoang3@gmail.com>
2019-09-09 10:23:37 -0400

    Added README and EscapeSequence box picture

---
+++ src/EscapeSequence.java
@@ -0,0 +1,9 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("Box");
+        System.out.println("*********");
+        for(int i = 0; i < 4; i++)
+            System.out.println("*\t*");
+        System.out.println("*********");
+    }
+}
